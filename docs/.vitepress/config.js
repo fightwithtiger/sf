c394092124e9d4cc75587d4312d3d8bc9202c11a
dengtuo <2284925818@qq.com>
2022-10-09 17:00:17 +0800

    'update'

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,8 +1,11 @@
+const { resolve } = require('path')
+
 module.exports = {
   title: 'fightwithtiger',
   description: 'A static website for some notes',
   base: '/sf/',
   markdown: {
     lineNumbers: true
-  }
+  },
+  outDir: resolve(__dirname, '../dist')
 }
